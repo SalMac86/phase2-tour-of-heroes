47bdc39c20a0d931f8d4e46c6f09d3083b8f8112
SalMac86 <salvatore.mccarty@gmail.com>
2017-07-24 02:29:46 +0000

    App with mock service Data - slowly

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain Insano' },
+  { id: 12, name: 'Nancy Botwin' },
+  { id: 13, name: 'Tuco' },
+  { id: 14, name: 'Saul Goodman' },
+  { id: 15, name: 'Jon Snow' },
+  { id: 16, name: 'The Doctor' },
+  { id: 17, name: 'Wonder Woman' },
+  { id: 18, name: 'Pam' },
+  { id: 19, name: 'Jim' },
+  { id: 20, name: 'Dwight' }
+];
