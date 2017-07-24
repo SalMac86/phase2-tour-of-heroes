2ffd706947b539f403da56e7c833c3815ae5dc8a
SalMac86 <salvatore.mccarty@gmail.com>
2017-07-24 23:00:49 +0000

    HTTP functioning

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -21,15 +21,19 @@ export class HeroesComponent implements OnInit {
   
   selectedHero: Hero;
   heroes: Hero[];
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+  
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
+  
   add(name: string): void {
     name = name.trim();
     if (!name) { return; }
@@ -37,6 +41,15 @@ export class HeroesComponent implements OnInit {
       .then(hero => {
         this.heroes.push(hero);
         this.selectedHero = null;
+      });
+  }
+  
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(()=> {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
       });
   }
 }
